c2f952c74757da3b5e6089f4528852b4b695b103
Vyacheslav <101479422+Juryxa@users.noreply.github.com>
2025-05-27 03:12:11 +0300

    some clear

--- clients/cloud-storage-web/src/app/api/services/CloudServices.ts
+++ clients/cloud-storage-web/src/app/api/services/CloudServices.ts
@@ -1,25 +1,11 @@
 import {AxiosResponse} from 'axios';
-import {CloudResponse, OneFileResponse} from "@/app/api/models/response/CloudResponse";
+import {CloudResponse} from "@/app/api/models/response/CloudResponse";
 import {cloudApi} from '@/app/api/http/cloud';
 
 export default class CloudService {
     static async getAllCloud(type: string): Promise<AxiosResponse<CloudResponse>> {
         return await cloudApi.get<CloudResponse>(`/files/all?type=${type}`);
     }
-
-
-    static async uploadFiles(filename: string, mimeType: string, category: "photo" | "video" | "text" | "unknown", encryptedBlob: ReadableStream<Uint8Array>) {
-        return await cloudApi.post<OneFileResponse>(`/files/one/encrypted`,  {
-            headers: {
-                'X-Orig-Filename': filename,
-                'X-Orig-Mime': mimeType,
-                'X-File-Category': category,
-                'Content-Type': 'application/octet-stream',
-            },
-            body: encryptedBlob,
-        });
-    }
-
 
     static async deleteFile(type: string, obj_id: string) {
         return await cloudApi.delete(`files/one?id=${obj_id}&type=${type}`);
